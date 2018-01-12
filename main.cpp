5409003181bbafc80f4eab7a3002922785f2967c
Jason Shi <jasonxshi2002@gmail.com>
2018-01-12 12:23:11 -0800

    fsdf

--- main.cpp
+++ main.cpp
@@ -6,5 +6,15 @@
 using namespace std;
 
 int main(){
-
+    char input[40];
+    cout<< "Type 'ADD' to add a student." << end;
+    cout << "Type 'PRINT' to print all stored students." << endl;
+    cout<< "Type 'DELETE' to delete a student." << endl;
+    cout << "Type 'QUIT' to exit" << endl;
+    cout << "Type 'AVERAGE' to print the average GPA" << endl;
+    while(1){
+    cout << "Enter a command:"<<endl;
+    cin.getline(input, 40);
+    
+    }
 }
